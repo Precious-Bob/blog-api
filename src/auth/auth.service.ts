61dc64990fe9ca39eb6bb9f6a998260a168acaad
Pr3ciousXX <preciousbobmanuel501@gmail.com>
2024-07-17 09:20:11 +0100

    fix: remove comments

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -32,28 +32,6 @@ export class AuthService {
     }
   }
 
-  // async signup({ email, firstName, lastName, password }: SignupDto) {
-  //   try {
-  //     const hash = await argon.hash(password);
-  //     const user = await this.prisma.user.create({
-  //       data: {
-  //         firstName,
-  //         lastName,
-  //         email,
-  //         hash,
-  //       },
-  //     });
-  //     return this.signToken(user.id, user.email);
-  //   } catch (e) {
-  //     if (e instanceof PrismaClientKnownRequestError) {
-  //       if (e.code === 'P2002') {
-  //         throw new ConflictException('Credentials already exists');
-  //       }
-  //     }
-  //     throw e;
-  //   }
-  // }
-
   async signin({ email, password }: signinDto) {
     try {
       const user = await this.userRepo.findOne({ where: { email } });
@@ -66,22 +44,7 @@ export class AuthService {
     } catch (e) {
       throw e;
     }
-    // async signin({ email, password }: SigninDto) {
-    //   // Find the user by email
-    //   const user = await this.prisma.user.findUnique({
-    //     where: {
-    //       email: email,
-    //     }/., ,
-    //   });
-    //   // If user doesn't exist, throw exception
-    //   if (!user) throw new NotFoundException('Credentials incorrect');
-    //   // Compare password
-    //   const validPassword = await argon.verify(user.hash, password);
-
-    //   // If password incorrect throw exception
-    //   if (!validPassword) throw new NotFoundException('Credentials incorrect');
-    //   return this.signToken(user.id, user.email);
-    // }
+   
   }
 
   async signToken(
@@ -100,21 +63,5 @@ export class AuthService {
       access_token: token,
     };
   }
-  // async signToken(
-  //   userId: string,
-  //   email: string,
-  // ): Promise<{ access_token: string }> {
-  //   const payload = {
-  //     sub: userId,
-  //     email,
-  //   };
-  //   const token = await this.jwt.signAsync(payload, {
-  //     expiresIn: '60m',
-  //     secret: this.config.get('JWT_SECRET'),
-  //   });
-
-  //   return {
-  //     access_token: token,
-  //   };
-  // }
+ 
 }
